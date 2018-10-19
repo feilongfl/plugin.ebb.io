58ecca698776986c2ba8c73db7ffda35332eb9a9
fei long <feilongphone@gmail.com>
2018-10-19 16:48:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ import xbmcplugin
 
 #lz-string
 from resources.lib import lzstring
+from resources.lib import encode as multipart_encode
+from resources.lib import streaminghttp
 x = lzstring.LZString()
 
 #init plugin
@@ -38,10 +40,17 @@ def Get(url):
     return urllib2.urlopen(req).read()
 
 def Post(url,params):
-    req = urllib2.Request(url)
     _params = urllib.urlencode(params)
+    req = urllib2.Request(url,_params)
     req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0)')
-    return urllib2.urlopen(req,_params).read()
+    return urllib2.urlopen(req).read()
+
+def Post(url,params,header):
+    _params = urllib.urlencode(params)
+    req = urllib2.Request(url,_params,header)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0)')
+    return urllib2.urlopen(req).read()
+
 
 def GetUtf16(url):
     # download pages
@@ -57,6 +66,11 @@ def GetUtf16(url):
 def PostForUtf16(url,params):
     page = Post(url,params)
     return x.decompressFromUTF16(page.decode('utf-8')).encode('utf-8')
+
+def PostForUtf16(url,params,header):
+    page = Post(url,params,header)
+    return x.decompressFromUTF16(page.decode('utf-8')).encode('utf-8')
+
 
 # decode
 def ttdecode(code):
@@ -91,6 +105,10 @@ if mode is None:
     # li = xbmcgui.ListItem(u'Search'.encode('utf-8'))
     # url = build_url({'mode': 'Search'})
     # xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+    li = xbmcgui.ListItem(u'INPUT ID'.encode('utf-8'))
+    url = build_url({'mode': 'V-IN'})
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
     # li = xbmcgui.ListItem(u'test'.encode('utf-8'))
     # li.setInfo("video",{})
@@ -155,11 +173,11 @@ elif mode[0] == 'video-info':
 
     jsonArr = json.loads(GetUtf16("https://ebb.io/_/season_list/" + id))
     if jsonArr['success'] == True:
-        for jsonSeason in jsonArr['list']['seasons']:
-            if jsonSeason['id'] == int(seasons):
-                for jsonEpisodes in jsonSeason['episodes']:
+        for jsonSeason in reversed(jsonArr['list']['seasons']):
+            # if jsonSeason['id'] == int(seasons):
+                for jsonEpisodes in reversed(jsonSeason['episodes']):
                     # print jsonEpisodes['id']
-                    li = xbmcgui.ListItem(jsonArr['list']['anime']['name_chi'] + jsonEpisodes['title'],thumbnailImage="https://ebb.io/_/poster/" + id + "x" + seasons)
+                    li = xbmcgui.ListItem(jsonArr['list']['anime']['name_chi'] + ' ' + jsonSeason['season_title'] + ' ' + jsonEpisodes['title'],thumbnailImage="https://ebb.io/_/poster/" + id + "x" + seasons)
                     li.setInfo("video",{
                         'director': jsonArr['list']['anime']['author'],
                         'title': jsonArr['list']['anime']['name_chi'] + jsonEpisodes['title'],
@@ -176,9 +194,19 @@ elif mode[0] == 'video-info':
 elif mode[0] == 'Search':
     # https://ebb.io/_/search
     searchStr = 'dao'
-    jsonArr = PostForUtf16("https://ebb.io/_/search",{'query': searchStr})
+    datagen, headers = multipart_encode({"image1": searchStr})
+    jsonArr = PostForUtf16("https://ebb.io/_/search", datagen, headers)
     print jsonArr
     xbmcgui.Dialog().ok(u'is developing'.encode('utf-8'),u'is developing'.encode('utf-8'))
+
+elif mode[0] == 'V-IN':
+    s = xbmcgui.Dialog().input('INPUT ID:')
+    li = xbmcgui.ListItem(s)
+    id = re.search(r'(\d+)x(\d+)', s).group(1)
+    sid = re.search(r'(\d+)x(\d+)', s).group(2)
+    url = build_url({'mode': 'video-info', 'id': id, 'seasons': sid})
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
 
 else:
     xbmcgui.Dialog().ok(u'is developing'.encode('utf-8'),u'is developing'.encode('utf-8'))
